25eb6271c56e0f38fda4c0f70f68bf22b105fb86
Armaan2006 <69916704+Armaan2006@users.noreply.github.com>
2020-10-15 21:54:25 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Car-Racing-Game-Stage-1
-Creating in the play button with typing in different names : multiplayer
+# synchronousBallMovement
+Ball moving synchronously
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canvas;
+var gameState = 0;
+var playerCount = 0;
+var database;
+var form,player,game;
+
+function setup(){
+  database = firebase.database();
+  createCanvas(500,500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+
+}
